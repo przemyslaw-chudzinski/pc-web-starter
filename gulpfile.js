33cf592be44c2c524e573ad599d24b80402e1f6b
Przemysław Chudziński <przemyslaw-chudzinski@wp.pl>
2018-12-21 14:29:54 +0100

    working on project - before testing

---
+++ gulpfile.js
@@ -0,0 +1,4 @@
+require('./dev-scripts/sass');
+require('./dev-scripts/javascript');
+require('./dev-scripts/templates');
+require('./dev-scripts/server');
---
+++ src/data.js
@@ -0,0 +1,6 @@
+/**
+ * Global data what you can pass into views
+ */
+module.exports = {
+    test: "test something"
+};
